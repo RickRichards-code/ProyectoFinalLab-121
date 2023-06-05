9d8ccbfa237d19d8fe7d8e93955dc74adde56f30
Enrique julio Alvarado <rick.2099.xd@gmail.com>
2023-06-05 15:09:05 -0400

    interfaz

---
+++ Proyect/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace Proyect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PanelMenu = new System.Windows.Forms.Panel();
+            this.buttonProblema1 = new System.Windows.Forms.Button();
+            this.buttonBiblioteca = new System.Windows.Forms.Button();
+            this.buttonMuseo = new System.Windows.Forms.Button();
+            this.PanelText = new System.Windows.Forms.Panel();
+            this.PanelDashboard = new System.Windows.Forms.Panel();
+            this.buttonProblema2 = new System.Windows.Forms.Button();
+            this.buttonProblema3 = new System.Windows.Forms.Button();
+            this.buttonProblema4 = new System.Windows.Forms.Button();
+            this.buttonProblema5 = new System.Windows.Forms.Button();
+            this.textProblemas = new System.Windows.Forms.Label();
+            this.textTitulo = new System.Windows.Forms.Label();
+            this.textTitulo2 = new System.Windows.Forms.Label();
+            this.PanelMenu.SuspendLayout();
+            this.PanelText.SuspendLayout();
+            this.PanelDashboard.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PanelMenu
+            // 
+            this.PanelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(17)))), ((int)(((byte)(26)))));
+            this.PanelMenu.Controls.Add(this.buttonProblema5);
+            this.PanelMenu.Controls.Add(this.buttonProblema4);
+            this.PanelMenu.Controls.Add(this.buttonProblema3);
+            this.PanelMenu.Controls.Add(this.buttonProblema2);
+            this.PanelMenu.Controls.Add(this.buttonProblema1);
+            this.PanelMenu.Controls.Add(this.buttonBiblioteca);
+            this.PanelMenu.Controls.Add(this.buttonMuseo);
+            this.PanelMenu.Controls.Add(this.PanelText);
+            this.PanelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.PanelMenu.Location = new System.Drawing.Point(0, 0);
+            this.PanelMenu.Name = "PanelMenu";
+            this.PanelMenu.Size = new System.Drawing.Size(267, 578);
+            this.PanelMenu.TabIndex = 0;
+            // 
+            // buttonProblema1
+            // 
+            this.buttonProblema1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(45)))), ((int)(((byte)(63)))));
+            this.buttonProblema1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonProblema1.FlatAppearance.BorderSize = 0;
+            this.buttonProblema1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonProblema1.Font = new System.Drawing.Font("Roboto", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonProblema1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(115)))), ((int)(((byte)(218)))), ((int)(((byte)(202)))));
+            this.buttonProblema1.Location = new System.Drawing.Point(0, 254);
+            this.buttonProblema1.Name = "buttonProblema1";
+            this.buttonProblema1.Size = new System.Drawing.Size(267, 45);
+            this.buttonProblema1.TabIndex = 3;
+            this.buttonProblema1.Text = "Problema 1";
+            this.buttonProblema1.UseVisualStyleBackColor = false;
+            this.buttonProblema1.Click += new System.EventHandler(this.buttonProblema1_Click);
+            // 
+            // buttonBiblioteca
+            // 
+            this.buttonBiblioteca.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(45)))), ((int)(((byte)(63)))));
+            this.buttonBiblioteca.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonBiblioteca.FlatAppearance.BorderSize = 0;
+            this.buttonBiblioteca.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonBiblioteca.Font = new System.Drawing.Font("Roboto", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBiblioteca.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(115)))), ((int)(((byte)(218)))), ((int)(((byte)(202)))));
+            this.buttonBiblioteca.Location = new System.Drawing.Point(0, 209);
+            this.buttonBiblioteca.Name = "buttonBiblioteca";
+            this.buttonBiblioteca.Size = new System.Drawing.Size(267, 45);
+            this.buttonBiblioteca.TabIndex = 2;
+            this.buttonBiblioteca.Text = "Biblioteca";
+            this.buttonBiblioteca.UseVisualStyleBackColor = false;
+            this.buttonBiblioteca.Click += new System.EventHandler(this.buttonBiblioteca_Click);
+            // 
+            // buttonMuseo
+            // 
+            this.buttonMuseo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(45)))), ((int)(((byte)(63)))));
+            this.buttonMuseo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonMuseo.FlatAppearance.BorderSize = 0;
+            this.buttonMuseo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMuseo.Font = new System.Drawing.Font("Roboto", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMuseo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(115)))), ((int)(((byte)(218)))), ((int)(((byte)(202)))));
+            this.buttonMuseo.Location = new System.Drawing.Point(0, 164);
+            this.buttonMuseo.Name = "buttonMuseo";
+            this.buttonMuseo.Size = new System.Drawing.Size(267, 45);
+            this.buttonMuseo.TabIndex = 1;
+            this.buttonMuseo.Text = "Museo";
+            this.buttonMuseo.UseVisualStyleBackColor = false;
+            this.buttonMuseo.Click += new System.EventHandler(this.buttonMuseo_Click);
+            // 
+            // PanelText
+            // 
+            this.PanelText.Controls.Add(this.textTitulo2);
+            this.PanelText.Controls.Add(this.textTitulo);
+            this.PanelText.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PanelText.Location = new System.Drawing.Point(0, 0);
+            this.PanelText.Name = "PanelText";
+            this.PanelText.Size = new System.Drawing.Size(267, 164);
+            this.PanelText.TabIndex = 0;
+            this.PanelText.Paint += new System.Windows.Forms.PaintEventHandler(this.PanelText_Paint);
+            // 
+            // PanelDashboard
+            // 
+            this.PanelDashboard.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(45)))), ((int)(((byte)(63)))));
+            this.PanelDashboard.Controls.Add(this.textProblemas);
+            this.PanelDashboard.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PanelDashboard.Location = new System.Drawing.Point(267, 0);
+            this.PanelDashboard.Name = "PanelDashboard";
+            this.PanelDashboard.Size = new System.Drawing.Size(884, 578);
+            this.PanelDashboard.TabIndex = 1;
+            // 
+            // buttonProblema2
+            // 
+            this.buttonProblema2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(45)))), ((int)(((byte)(63)))));
+            this.buttonProblema2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonProblema2.FlatAppearance.BorderSize = 0;
+            this.buttonProblema2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonProblema2.Font = new System.Drawing.Font("Roboto", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonProblema2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(115)))), ((int)(((byte)(218)))), ((int)(((byte)(202)))));
+            this.buttonProblema2.Location = new System.Drawing.Point(0, 299);
+            this.buttonProblema2.Name = "buttonProblema2";
+            this.buttonProblema2.Size = new System.Drawing.Size(267, 45);
+            this.buttonProblema2.TabIndex = 4;
+            this.buttonProblema2.Text = "Problema 2";
+            this.buttonProblema2.UseVisualStyleBackColor = false;
+            this.buttonProblema2.Click += new System.EventHandler(this.buttonProblema2_Click);
+            // 
+            // buttonProblema3
+            // 
+            this.buttonProblema3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(45)))), ((int)(((byte)(63)))));
+            this.buttonProblema3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonProblema3.FlatAppearance.BorderSize = 0;
+            this.buttonProblema3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonProblema3.Font = new System.Drawing.Font("Roboto", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonProblema3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(115)))), ((int)(((byte)(218)))), ((int)(((byte)(202)))));
+            this.buttonProblema3.Location = new System.Drawing.Point(0, 344);
+            this.buttonProblema3.Name = "buttonProblema3";
+            this.buttonProblema3.Size = new System.Drawing.Size(267, 45);
+            this.buttonProblema3.TabIndex = 5;
+            this.buttonProblema3.Text = "Problema 3";
+            this.buttonProblema3.UseVisualStyleBackColor = false;
+            this.buttonProblema3.Click += new System.EventHandler(this.buttonProblema3_Click);
+            // 
+            // buttonProblema4
+            // 
+            this.buttonProblema4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(45)))), ((int)(((byte)(63)))));
+            this.buttonProblema4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonProblema4.FlatAppearance.BorderSize = 0;
+            this.buttonProblema4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonProblema4.Font = new System.Drawing.Font("Roboto", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonProblema4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(115)))), ((int)(((byte)(218)))), ((int)(((byte)(202)))));
+            this.buttonProblema4.Location = new System.Drawing.Point(0, 389);
+            this.buttonProblema4.Name = "buttonProblema4";
+            this.buttonProblema4.Size = new System.Drawing.Size(267, 45);
+            this.buttonProblema4.TabIndex = 6;
+            this.buttonProblema4.Text = "Problema 4";
+            this.buttonProblema4.UseVisualStyleBackColor = false;
+            this.buttonProblema4.Click += new System.EventHandler(this.buttonProblema4_Click);
+            // 
+            // buttonProblema5
+            // 
+            this.buttonProblema5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(45)))), ((int)(((byte)(63)))));
+            this.buttonProblema5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonProblema5.FlatAppearance.BorderSize = 0;
+            this.buttonProblema5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonProblema5.Font = new System.Drawing.Font("Roboto", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonProblema5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(115)))), ((int)(((byte)(218)))), ((int)(((byte)(202)))));
+            this.buttonProblema5.Location = new System.Drawing.Point(0, 434);
+            this.buttonProblema5.Name = "buttonProblema5";
+            this.buttonProblema5.Size = new System.Drawing.Size(267, 45);
+            this.buttonProblema5.TabIndex = 7;
+            this.buttonProblema5.Text = "Problema 5";
+            this.buttonProblema5.UseVisualStyleBackColor = false;
+            this.buttonProblema5.Click += new System.EventHandler(this.buttonProblema5_Click);
+            // 
+            // textProblemas
+            // 
+            this.textProblemas.AutoSize = true;
+            this.textProblemas.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F);
+            this.textProblemas.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.textProblemas.Location = new System.Drawing.Point(53, 56);
+            this.textProblemas.Name = "textProblemas";
+            this.textProblemas.Size = new System.Drawing.Size(130, 22);
+            this.textProblemas.TabIndex = 0;
+            this.textProblemas.Text = "                        ";
+            // 
+            // textTitulo
+            // 
+            this.textTitulo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textTitulo.AutoSize = true;
+            this.textTitulo.Font = new System.Drawing.Font("Roboto", 10.8F);
+            this.textTitulo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(119)))), ((int)(((byte)(142)))));
+            this.textTitulo.Location = new System.Drawing.Point(55, 56);
+            this.textTitulo.Name = "textTitulo";
+            this.textTitulo.Size = new System.Drawing.Size(150, 22);
+            this.textTitulo.TabIndex = 0;
+            this.textTitulo.Text = "Proyecto Lab-121";
+            this.textTitulo.Click += new System.EventHandler(this.textTitulo_Click);
+            // 
+            // textTitulo2
+            // 
+            this.textTitulo2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textTitulo2.AutoSize = true;
+            this.textTitulo2.Font = new System.Drawing.Font("Roboto", 10.8F);
+            this.textTitulo2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(119)))), ((int)(((byte)(142)))));
+            this.textTitulo2.Location = new System.Drawing.Point(46, 92);
+            this.textTitulo2.Name = "textTitulo2";
+            this.textTitulo2.Size = new System.Drawing.Size(178, 22);
+            this.textTitulo2.TabIndex = 1;
+            this.textTitulo2.Text = "Museos y Bibliotecas";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1151, 578);
+            this.Controls.Add(this.PanelDashboard);
+            this.Controls.Add(this.PanelMenu);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.PanelMenu.ResumeLayout(false);
+            this.PanelText.ResumeLayout(false);
+            this.PanelText.PerformLayout();
+            this.PanelDashboard.ResumeLayout(false);
+            this.PanelDashboard.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PanelMenu;
+        private System.Windows.Forms.Panel PanelDashboard;
+        private System.Windows.Forms.Panel PanelText;
+        private System.Windows.Forms.Button buttonMuseo;
+        private System.Windows.Forms.Button buttonProblema1;
+        private System.Windows.Forms.Button buttonBiblioteca;
+        private System.Windows.Forms.Button buttonProblema5;
+        private System.Windows.Forms.Button buttonProblema4;
+        private System.Windows.Forms.Button buttonProblema3;
+        private System.Windows.Forms.Button buttonProblema2;
+        private System.Windows.Forms.Label textProblemas;
+        private System.Windows.Forms.Label textTitulo;
+        private System.Windows.Forms.Label textTitulo2;
+    }
+}
+
---
+++ Proyect/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyect
+{
+    public partial class Form1 : Form 
+{
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // panel texto: Menu, logo ,etc
+        private void PanelText_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+
+        // segundo label bontones  museos, biblo, preguntas, etc.
+
+        //private void PanelButtons_Paint(object sender, PaintEventArgs e)
+        //{
+
+        //}
+        // butonsMuseo
+        private void buttonMuseo_Click(object sender, EventArgs e)
+        {
+            textProblemas.Text = "lista todos los museos.";
+        }
+
+        private void buttonBiblioteca_Click(object sender, EventArgs e)
+        {
+            textProblemas.Text = "listar todas las bibliotecas.";
+        }
+
+        private void buttonProblema1_Click(object sender, EventArgs e)
+        {
+            textProblemas.Text = "Problema 1: Mostrar cuantas museos hay en la zona X";
+        }
+
+        private void buttonProblema2_Click(object sender, EventArgs e)
+        {
+            textProblemas.Text = "Problema 2: Mostrar en que museo y/o biblioteca trabaja el empleado X.";
+        }
+
+        private void buttonProblema3_Click(object sender, EventArgs e)
+        {
+            textProblemas.Text = "Problema 3: enunciad problema 3 jsjs...";
+        }
+
+        private void buttonProblema4_Click(object sender, EventArgs e)
+        {
+            textProblemas.Text = "Problema 4: enunciad problema 4 jsjs...";
+        }
+
+        private void buttonProblema5_Click(object sender, EventArgs e)
+        {
+            textProblemas.Text = "Problema 5: problema 5 jsjs...";
+        }
+
+        private void textTitulo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+    }
+
+}
